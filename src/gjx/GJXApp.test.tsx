89b77001c20e7a37777a176fdfb7d60f7a470df7
Rokoucha <6058487+rokoucha@users.noreply.github.com>
2024-06-08 19:54:52 +0900

    remove broken test

--- src/gjx/GJXApp.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import GJXApp from "./GJXApp";
-
-test("renders learn react link", () => {
-  render(<GJXApp />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
